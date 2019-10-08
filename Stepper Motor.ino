6ef07454d7536d4d028e8549121d51a4011f3709
cclores1 <55910085+cclores1@users.noreply.github.com>
2019-10-07 20:16:13 -0700

    Update Stepper Motor.ino

--- Stepper Motor.ino
+++ Stepper Motor.ino
@@ -6,16 +6,19 @@
 //1.8deg per step
 //increase by 200^n to increase resolution
 //400/800/1600
-const int stepsPerRevolution = 200;
+const int stepsPerRevolution = 400;
 
 
 //initializes pins 8-11 on arduino
-Stepper myStepper(stepsPerRevolution, 8, 9,10, 11);
+Stepper Step1(stepsPerRevolution, 8, 9,10, 11);
+Stepper Step2(stepsPerRevolution, 2, 3,4, 5);
+
 
 void setup()
 {
   //set speed at 60rpm
-  myStepper.setSpeed(60);
+  Step1.setSpeed(60);
+  Step2.setSpeed(60);
   //Initializes serial monitor
   Serial.begin(9600);
 }
@@ -24,12 +27,16 @@ void loop()
 {
   //one revolution in clockwise and prints on monitor then delay for half a second
   Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
+  Step1.step(stepsPerRevolution);
+  delay(100);
+  Step1.step(stepsPerRevolution);
   delay(500);
   
   
   //one revolution in CCW and prints on monitor then delay for half a second
   Serial.Println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
+  Step1.step(-stepsPerRevolution);
+  delay(100);
+  Step2.step(-stepsPerRevolution);
   delay(500);
 }
